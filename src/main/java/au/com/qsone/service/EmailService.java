928708cc9d4c23cb79a913f7acf9eac71803e5b9
musudikiran <musudi.kiran@gmail.com>
2021-11-01 20:07:01 +0100

    commented email impl

--- src/main/java/au/com/qsone/service/EmailService.java
+++ src/main/java/au/com/qsone/service/EmailService.java
@@ -11,25 +11,25 @@ import javax.mail.MessagingException;
  * Created by Olga on 8/22/2016.
  */
 public interface EmailService {
-    void sendSimpleMessage(String to,
-                           String subject,
-                           String text);
-    void sendSimpleMessageUsingTemplate(String to,
-                                        String subject,
-                                        String ...templateModel);
-    void sendMessageWithAttachment(String to,
-                                   String subject,
-                                   String text,
-                                   String pathToAttachment);
-    
-    void sendMessageUsingThymeleafTemplate(String to,
-                                           String subject,
-                                           Map<String, Object> templateModel) 
-            throws IOException, MessagingException;
+//    void sendSimpleMessage(String to,
+//                           String subject,
+//                           String text);
+//    void sendSimpleMessageUsingTemplate(String to,
+//                                        String subject,
+//                                        String ...templateModel);
+//    void sendMessageWithAttachment(String to,
+//                                   String subject,
+//                                   String text,
+//                                   String pathToAttachment);
+//    
+//    void sendMessageUsingThymeleafTemplate(String to,
+//                                           String subject,
+//                                           Map<String, Object> templateModel) 
+//            throws IOException, MessagingException;
 
-	/*
-	 * void sendMessageUsingFreemarkerTemplate(String to, String subject,
-	 * Map<String, Object> templateModel) throws IOException, TemplateException,
-	 * MessagingException;
-	 */
+	
+//	  void sendMessageUsingFreemarkerTemplate(String to, String subject,
+//	  Map<String, Object> templateModel) throws IOException, TemplateException,
+//	  MessagingException;
+	 
 }
